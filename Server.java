18d321dc936b6fd354bbe9713ee8702f9da9a543
danutHij1414 <b00077321@student.itb.ie>
2017-03-06 18:50:05 +0000

    server class updated

--- Server.java
+++ Server.java
@@ -1,4 +1,102 @@
 
-public class Server {
+import java.io.*;
+import java.net.*;
 
+public class Server {
+	private ServerSocket serverSocket = null;
+	private Socket socket = null;
+	private InputStream inStream = null;
+	private OutputStream outStream = null;
+
+	public Server() {
+
+	}
+
+	public void createSocket() {
+		try {
+			ServerSocket serverSocket = new ServerSocket(6066);
+			while (true) {
+				socket = serverSocket.accept();
+				inStream = socket.getInputStream();
+				System.out.println("Connected\n");
+				createThreadThread();
+				createWriteThread();
+			}
+		} catch (IOException io) {
+			io.printStackTrace();
+		}
+	}
+
+	public void createThreadThread()
+	{
+		Thread readThread = new Thread()
+		{
+			public void run()
+			{
+				while (socket.isConnected())
+				{
+					try {
+						byte[] readBuffer = new byte[200];
+						int num = inStream.read(readBuffer);
+						
+						if (num > 0)
+						{
+							byte[] arrayBytes = new byte[num];
+							System.arraycopy(readBuffer, 0, arrayBytes, 0, num);
+							String recvedMessage = new String(arrayBytes, "UTF-8");
+							System.out.println("Received message :" +  recvedMessage);
+						} else {
+							notify();
+						}
+					} catch (SocketException se) {
+						System.exit(0);
+					} catch (IOException i) {
+						i.printStackTrace();
+					}
+				}
+			}
+		};
+		readThread.setPriority(Thread.MAX_PRIORITY);
+		readThread.start();
+	}
+
+	public void createWriteThread()
+	{
+		Thread writeThread = new Thread()
+		{
+			public void run()
+			{
+
+				while (socket.isConnected())
+				{
+					try {
+						BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+						sleep(100);
+						String typedMessage = inputReader.readLine();
+						if (typedMessage != null && typedMessage.length() > 0)
+						{
+							synchronized (socket) {
+								outStream.write(typedMessage.getBytes("UTF-8"));
+								sleep(100);
+							}
+						}
+					} catch (IOException i) {
+						i.printStackTrace();
+					} catch (InterruptedException ie) {
+						ie.printStackTrace();
+					}
+
+				}
+			}
+		};
+		writeThread.setPriority(Thread.MAX_PRIORITY);
+		writeThread.start();
+
+	}
+
+	public static void main(String[] args) 
+	{
+		Server chatServer = new Server();
+		chatServer.createSocket();
+	}
 }
