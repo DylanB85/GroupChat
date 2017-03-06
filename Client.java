d1fa85fbb4a124326b7a8e2b2423a07e3cf35778
Dylan Byrne <b00082154@Student.itb.ie>
2017-03-06 18:25:09 +0000

    Client side code

---
+++ Client.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class Client {
+	private Socket chatSocket = null;
+	private InputStream input = null;
+	private OutputStream output = null;
+	
+	public void chatApp(){
+		try{
+			chatSocket = new Socket("LocalHost", 6066);
+			System.out.println("You are now connected to the chat");
+			input = chatSocket.getInputStream();
+			output = chatSocket.getOutputStream();
+			reader();
+			writer();
+		} catch (UnknownHostException h){
+			h.printStackTrace();
+		} catch (IOException io){
+			io.printStackTrace();
+		}
+	}
+	
+	public void reader(){
+		Thread reads = new Thread(){
+			public void run(){
+				while(chatSocket.isConnected()){
+					try{
+						byte[] readBuffer = new byte[200];
+						int number = input.read(readBuffer);
+						
+						if(number > 0){
+							byte[] arrayByte = new byte[number];
+							System.arraycopy(readBuffer, 0, arrayByte, 0, number);
+							String recievedMessages = new String(arrayByte, "UTF-8");
+							System.out.println("Messages Recieved" + recievedMessages);
+							/* else{
+							 * notify();
+							 * }
+							 */
+						}
+					}catch(SocketException se){
+						System.exit(0);
+					} catch(IOException ioe){
+						ioe.printStackTrace();
+					}
+				}
+			}
+		};
+		reads.setPriority(Thread.MAX_PRIORITY);
+		reads.start();
+	}
+	
+	public void writer(){
+		Thread write = new Thread(){
+			public void run(){
+				while(chatSocket.isConnected()){
+					try{
+						BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+						sleep(100);
+						String typedMessages = inputReader.readLine();
+						if(typedMessages != null && typedMessages.length()>0){
+							synchronized(chatSocket){
+								output.write(typedMessages.getBytes("UTF-8"));
+								sleep(100);
+							}
+						};
+					}catch(IOException ioe){
+						ioe.printStackTrace();
+					} catch (InterruptedException ie){
+						ie.printStackTrace();
+					}
+				}
+			}
+		};
+		write.setPriority(Thread.MAX_PRIORITY);
+		write.start();
+	}
+	
+	public static void main(String[]args) throws Exception{
+		Client chatClient = new Client();
+		chatClient.chatApp();
+	}
+
+}
